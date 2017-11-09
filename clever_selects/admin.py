e61521da3e27cf5e245d493da3e04895751f92e2
Jermel Babb <developers@octane5.net>
2017-11-09 11:02:57 -0500

    Fix super() calls to be Python 2.7 compatible

--- clever_selects/admin.py
+++ clever_selects/admin.py
@@ -1,5 +1,7 @@
 
-class ChainedSelectAdminMixin(object):
+from django.contrib.admin import ModelAdmin
+
+class ChainedSelectAdminMixin(ModelAdmin):
 
     def get_form(self, request, obj=None, **kwargs):
         form = super(ChainedSelectAdminMixin, self).get_form(request, obj, **kwargs)
