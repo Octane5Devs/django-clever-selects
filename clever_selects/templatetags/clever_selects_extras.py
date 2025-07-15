65341c4c2a6806cee3c57b6aebf0bf709e373684
jlbabb <jlbabb@gmail.com>
2025-07-15 13:42:17 -0400

    Make compatible with Django 5.2

--- clever_selects/templatetags/clever_selects_extras.py
+++ clever_selects/templatetags/clever_selects_extras.py
@@ -3,12 +3,12 @@ import logging
 from django import template
 from django.middleware.csrf import get_token
 from django.conf import settings
-from django.core.files.storage import get_storage_class
+from django.core.files.storage import import_storage_class
 from django.utils.safestring import mark_safe
 
 from clever_selects import VERSION
 
-staticfiles_storage = get_storage_class(settings.STATICFILES_STORAGE)()
+staticfiles_storage = import_storage_class(settings.STATICFILES_STORAGE)()
 
 register = template.Library()
 
